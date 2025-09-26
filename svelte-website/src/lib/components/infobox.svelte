db96d661cc8343545193eef53c51aaa88810478b
nassory <ethangriffithspersonal@gmail.com>
2025-09-26 21:02:40 +1000

    added svelte + vite project

---
+++ svelte-website/src/lib/components/infobox.svelte
@@ -0,0 +1,12 @@
+<section id="InfoBox" class="displace" style="width: 600px; height: 600px;">
+        <h5>woa</h5>
+        <br><br>
+        <p>There are many variations of passages of Lorem Ipsum available, 
+            but the majority have suffered alteration in some form, 
+            by injected humour, or randomised words which don't look even 
+            slightly believable. If you are going to use a passage of Lorem Ipsum,
+        </p>
+        <br><br><br><br>
+        <p>links</p>
+        <br><br>
+</section>
